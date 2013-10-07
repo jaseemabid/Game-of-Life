15a3cff333ed70632d84ff3a88e796afd362e43c
Jaseem Abid <jaseemabid@gmail.com>
2013-10-07 20:16:27 +0530

    + defun game-of-life

--- convey.el
+++ convey.el
@@ -2,7 +2,6 @@
 ;; Author: Jaseem Abid <jaseemabid@gmail.com>
 
 (setq
- buffer (get-buffer-create "game-of-life")
  gen '((0 0 0 1 0 0 0 1)
 	   (0 1 0 0 0 0 0 1)
 	   (0 0 0 1 0 0 0 1)
@@ -10,19 +9,22 @@
 	   ))
 
 (defun show-gen ()
-  (progn
-	  (switch-to-buffer buffer)
-	  (erase-buffer)
-
-	  ;; Display grid
-	  (loop for row in gen
-			do
-			(insert (concat "\n"
-							(loop for i in row
-								  collect (if (= i 0) 32 95 )
-								  )))
-			)
-	  )
+  (erase-buffer)
+  ;; Display grid
+  (loop for row in gen
+		do
+		(insert (concat "\n"
+						(loop for i in row
+							  collect (if (= i 0) 32 95 )
+							  )))
+		)
   )
 
-(show-gen)
+(defun game-of-life ()
+  "Game of life"
+  (interactive)
+  (switch-to-buffer (get-buffer-create "game-of-life"))
+  (show-gen)
+  )
+
+(game-of-life)
